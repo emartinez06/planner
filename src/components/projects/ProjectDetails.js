4916e7f33f14e56b9f293b302010cdbc83cc055c
emartinez06 <mail@devmartinez.net>
2019-01-29 14:51:00 -0400

    Planner: Addition of Firebase to project. Update to package.json file to use another versions. Update on several files to handle the state and connections to firebase.

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,22 +1,49 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {firestoreConnect} from 'react-redux-firebase';
+import {compose} from 'redux';
 
-const ProjectDetails = (props)=>{
-    //console.log(props);
+const ProjectDetails = (props) => {
+    console.log(props);
+    const {project} = props;
     const id = props.match.params.id;
-    return(
-        <div className="container section project-details">
-            <div className="card z-depth-0">
-                <div className="card-content">
-                    <span className="card-title">Project Title - {id}</span>
-                    <p>Lorem ipsum sit amet contecteus</p>
-                </div>
-                <div className="card-action grey lighten-4 grey-text">
-                    <div>Posted by Tenebris</div>
-                    <div>January 21, 2019</div>
+    if (project) {
+        return (
+            <div className="container section project-details">
+                <div className="card z-depth-0">
+                    <div className="card-content">
+                        <span className="card-title">{project.title}</span>
+                        <p>{project.content}</p>
+                    </div>
+                    <div className="card-action grey lighten-4 grey-text">
+                        <div>Posted by {project.authorFirstName} {project.authorLastName}</div>
+                        <div>January 21, 2019</div>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    } else {
+        return (
+            <div className="container center">
+                <p>Loading project...</p>
+            </div>
+        )
+    }
 };
 
-export default ProjectDetails;
+const mapStateToProps = (state, singleProps) => {
+    //console.log(state);
+    const id = singleProps.match.params.id;
+    const projects = state.firestore.data.projects;
+    const project = projects ? projects[id] : null;
+    return {
+        project: project
+    }
+};
+
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        {collection: 'projects'}
+    ])
+)(ProjectDetails);
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,12 +1,21 @@
 const initState = {
-    projects:[
-        {id:'1',title:'Organizing project structure', content:'Modularize app into components'},
-        {id:'2',title:'Organizing database structure', content:'Reorganize database tables'},
-        {id:'3',title:'Optimize files', content:'Optimize images and minify all files'}
+    projects: [
+        {id: '1', title: 'Organizing project structure', content: 'Modularize app into components'},
+        {id: '2', title: 'Organizing database structure', content: 'Reorganize database tables'},
+        {id: '3', title: 'Optimize files', content: 'Optimize images and minify all files'}
     ]
 };
 const projectReducer = (state = initState, action) => {
-    return state;
+    switch (action.type) {
+        case 'CREATE_PROJECT':
+            console.log('Created project', action.project);
+            return state;
+        case 'CREATE_PROJECT_ERROR':
+            console.log('Created project error',action.err);
+            return state;
+        default:
+            return state;
+    }
 };
 
 export default projectReducer;
